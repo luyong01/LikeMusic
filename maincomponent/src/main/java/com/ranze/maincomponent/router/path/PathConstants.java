b3894c8582816bd48d8613a08c5306162a454c8c
luyong01 <luyong01@baidu.com>
2018-02-09 21:08:05 +0800

    Add navigation activity

--- maincomponent/src/main/java/com/ranze/maincomponent/router/path/PathConstants.java
+++ maincomponent/src/main/java/com/ranze/maincomponent/router/path/PathConstants.java
@@ -7,10 +7,10 @@ import android.view.View;
  */
 
 public class PathConstants {
-    private static final String ROOT = "/main";
+    private static final String ROOT = "/maincomponent_main";
     private static final String VIEW = "/view";
     private static final String SERVICE = "/service";
 
-    public static final String VIEW_MAIN = ROOT + VIEW + "/main";
-    public static final String SERVICE_MAIN = ROOT + SERVICE + "/main";
+    public static final String VIEW_MAIN = ROOT + VIEW + "/maincomponent_main";
+    public static final String SERVICE_MAIN = ROOT + SERVICE + "/maincomponent_main";
 }
